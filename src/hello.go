7a83d9aceaef82a4f76982dc790504df524af442
Kevin Olfert <kolfert@arrisi.com>
2021-02-13 10:19:32 -1000

    Added module time

--- src/hello.go
+++ src/hello.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 
 	"rsc.io/quote"
+
+	"time"
 )
 
 func main() {
 	//fmt.Println("\nHello beautiful blockchain world!\n")
-	fmt.Println(quote.Go())
+	fmt.Println("")
+	fmt.Println(time.Now(), "---", quote.Go(), "\n")
+	//fmt.Println(quote.Go(), "\n")
 }
