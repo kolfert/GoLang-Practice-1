69177ff43abbba0074ff549429e8354cc6bdc399
Kevin Olfert <kolfert@arrisi.com>
2021-02-13 10:22:55 -1000

    Added code comments

--- src/hello.go
+++ src/hello.go
@@ -11,6 +11,7 @@ import (
 func main() {
 	//fmt.Println("\nHello beautiful blockchain world!\n")
 	fmt.Println("")
-	fmt.Println(time.Now(), "---", quote.Go(), "\n")
+	fmt.Println(time.Now(), "---", quote.Go()) // Adding \n here causes complaints
+	fmt.Println("")                            // pre-compiler omplalins when adding a new line, so we just Println with nothing.
 	//fmt.Println(quote.Go(), "\n")
 }
